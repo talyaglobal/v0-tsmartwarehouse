405f771bdaf8b4e2a7f4dcb56c452b829de7de5f
Batuhan Saygin <batuhan@tsmart.ai>
2025-12-23 14:35:09 -0500

    ok

--- tests/utils/test-utils.tsx
+++ tests/utils/test-utils.tsx
@@ -1,5 +1,5 @@
 import React, { type ReactElement } from 'react'
-import { render, type RenderOptions } from '@testing-library/react'
+import { render as baseRender, type RenderOptions } from '@testing-library/react'
 import { ThemeProvider } from 'next-themes'
 
 // Custom render function that includes providers
@@ -11,12 +11,14 @@ function AllTheProviders({ children }: { children: React.ReactNode }) {
   )
 }
 
+// Custom render function
 const customRender = (
   ui: ReactElement,
   options?: Omit<RenderOptions, 'wrapper'>,
-) => render(ui, { wrapper: AllTheProviders, ...options })
+) => baseRender(ui, { wrapper: AllTheProviders, ...options })
 
-// Re-export everything
+// Re-export everything from @testing-library/react (including screen)
 export * from '@testing-library/react'
-export { customRender as render, screen } from '@testing-library/react'
+// Export our custom render (this overrides the default render from the wildcard export)
+export { customRender as render }
 
